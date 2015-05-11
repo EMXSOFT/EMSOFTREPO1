7572b0d31761a00eecd0500166a795cba0af71ef
EMSOFT12 <anyaeu1@gmail.com>
2015-05-11 18:24:53 +0100

    add new files

---
+++ POM_COVER_PROJECT/src/Test/Encapsulation/School2.java
@@ -0,0 +1,9 @@
+package Test.Encapsulation;
+
+public class School2 {
+	
+	public void register(){
+		System.out.println("the register for the class");
+	}
+
+}
---
+++ POM_COVER_PROJECT/src/Test/Encapsulation/School3.java
@@ -0,0 +1,10 @@
+package Test.Encapsulation;
+
+public class School3 {
+	
+	public void register(){
+		
+		System.out.print("print students names");
+	}
+
+}
